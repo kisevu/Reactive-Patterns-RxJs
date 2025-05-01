5a5ea3cf3486aca90d11412fe07bac1c446492d5
ameda-kevin <amedakevin@gmail.com>
2025-05-01 10:06:04 +0300

    Signals as a replacement to Zone.js which does entire component tree checks when change has been detected as opposed to signals which narrows down to the affected components and re-renders the ui smartly

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, OnInit, computed, effect } from '@angular/core';
 import { SharedDataService } from '../../services/shared/shared-data.service';
 import { AsyncPipe } from '@angular/common';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-details',
@@ -9,13 +10,45 @@ import { AsyncPipe } from '@angular/common';
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
 
 
   sharedDataService = inject(SharedDataService);
 
   selectedRecipe$ = this.sharedDataService.selectedRecipe$;
 
+  counter = signal(0);
 
+   /* Creating signals  using constructor  function */
+
+   name = signal<string>('Kevin');
+   currencies = signal(['EURO','DOLLAR','JAPANESE YEN','STERLING']);
+   recipes = signal<Recipe>({title:'tacos',description: 'American cuisine'});
+   firstName = signal('Kevin');
+   secondName = signal('Ameda');
+
+   conjoined = computed( ()=> `${this.firstName()} ${this.secondName()}` );
+
+
+
+   ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.recipes.set({title: 'Chapati',description: 'Kenyan dish'});
+    this.name.update(data=>  "Name: "+ data);
+    console.log(this.conjoined());
+   }
+
+
+   constructor(){
+    effect(()=> {
+      console.log('The updated value is ', this.counter());
+    });
+   }
+
+
+   update() {
+    this.counter.update((current)=> current + 1);
+   }
 
 }
