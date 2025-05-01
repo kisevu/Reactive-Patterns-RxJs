05aa98c7cd1a0d62c71ee2a48297bc063540547b
ameda-kevin <amedakevin@gmail.com>
2025-05-01 11:48:38 +0300

    working with the toObservable() and toSignal() and drawing thier xtcs and checking the comparison betweeen asObservables() and toObservables()

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -2,6 +2,9 @@ import { Component, inject, signal, OnInit, computed, effect } from '@angular/co
 import { SharedDataService } from '../../services/shared/shared-data.service';
 import { AsyncPipe } from '@angular/common';
 import { Recipe } from '../../models/recipe.model';
+import { BehaviorSubject, delay, of, tap } from 'rxjs';
+
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-details',
@@ -19,6 +22,10 @@ export class DetailsComponent implements OnInit {
 
   counter = signal(0);
 
+
+
+  values$ = of(10,20,30).pipe(delay(5));
+  valuesAsSignal = toSignal(this.values$, { initialValue: 0 });
    /* Creating signals  using constructor  function */
 
    name = signal<string>('Kevin');
@@ -41,14 +48,58 @@ export class DetailsComponent implements OnInit {
 
 
    constructor(){
-    effect(()=> {
-      console.log('The updated value is ', this.counter());
-    });
+    // effect(()=> {
+    //   console.log('The updated value is ', this.counter());
+    // });
+      // effect( ()=> console.log(this.valuesAsSignal()) );
+
+      this.value.asObservable().pipe(
+        tap(x => console.log(`The value is: ${x}`))
+      ).subscribe();
+
+      this.value.next(20);
+      this.value.next(30);
+
+      toObservable(this.value2).pipe(
+        tap(x=>console.log(`The value 2 is : ${x}`))
+      ).subscribe();
+
+      this.value2.set(20);
+      this.value2.set(30);
+
    }
 
 
    update() {
     this.counter.update((current)=> current + 1);
    }
+
+
+   value = new BehaviorSubject(10);
+
+   value2 = signal(10);
+
+
+   /*
+   Both Signals and Observables are not mutually exclusive and we learn that they can be used together.
+   We have @angular/core/rxjs-interop package which includes toSignal() and toObservable()
+   toSignal() automatically subscribes to the defined Observable  and unsubscribes  when the component or service that calls toSignal()
+   is destroyed.
+   We do not manage subscriptions and this concept remind us of the asyncpipe with the only difference being,
+   that async pipe is used with the Observables in templates but Signal is flexible and is available everywhere else.
+   Signals, always has an initial value which reps an Signal value until the Observable is emitted.
+
+   valueAsSignal = toSignal(this.value$, {initial})
+
+
+   toObservable() -  allows to convert a Signal into an Observable.
+   Whenever the signal value changes, Observable automatically  emits a notification with the new value.
+   This allows to easily trigger  your async operation based on updated Signal data.
+   toObservable() uses effects under the hood to track the Signal's value and emit the latest  value to the Observable
+
+   toObservable() functions a little bit different from the asObservable() as  Signal change notifications and scheduled
+   rather than  immediately processed , as Observable notificatioins are
+   let's modifify the sample of value which uses asObservable()
+   */
 
 }
