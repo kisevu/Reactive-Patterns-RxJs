98a8ec2c5befc8881f41a5c780c5c9379f50fbe9
ameda-kevin <amedakevin@gmail.com>
2025-05-04 09:32:34 +0300

    Multicasting and unicasting, plain subject, replay subject and behavior subject and how best to use them and where

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductListComponent } from "../product-list/product-list.component";
+import { Observable, Subject } from 'rxjs';
+import { error } from 'jquery';
 
 @Component({
   selector: 'app-products',
@@ -8,6 +10,41 @@ import { ProductListComponent } from "../product-list/product-list.component";
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+
+  /* unicasting  cold observables */
+
+  observables$ = new Observable( observer => {
+    observer.next(Math.random());
+    observer.next(Math.random());
+  })
+
+
+  /* multi-casting hot observables */
+
+  value = Math.random();
+
+  observables1$ = new Observable( observer => {
+    observer.next(this.value);
+    observer.next(this.value);
+  })
+
+
+  /* Types of subjects:
+
+  * - Plainsubject - Subject();   - emission before subscription values get lost.
+  * - ReplaySubject - ReplaySubject(); - no subscription worry, cuz value will be retried.
+  * - BehaviorSubject - BehaviorSubject(); - no subscription worry but under condition that you will only get lastest emission.
+  */
+
+
+
+
+
+  ngOnInit(): void {
+  }
+
 
 }
+
